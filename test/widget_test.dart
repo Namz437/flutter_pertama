7f7ca3e1fe4927bbd982ee1647e601e84c8acdf1
Namz437 <155506964+Namz437@users.noreply.github.com>
2024-10-16 12:25:42 +0700

    commit oop class pertemuan 1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -83,55 +83,110 @@ import 'package:flutter_pertama/main.dart';
 
 // 
 
-void main() {
-//final keywoard = imutable / tidak bisa dirubah
-//const final
-//const
-//const String mahasiswa = "anam";
-//final
+// void main() {
+// //final keywoard = imutable / tidak bisa dirubah
+// //const final
+// //const
+// //const String mahasiswa = "anam";
+// //final
 
-  final String mahasiswa;
-  mahasiswa = "anam";
-  print(mahasiswa);
+//   final String mahasiswa;
+//   mahasiswa = "anam";
+//   print(mahasiswa);
 
-//null safety
-//? ! late
-//? digunakan dia boleh null
-//String? jurusan;
-// Untuk di isi nanti
-  late String jurusan;
+// //null safety
+// //? ! late
+// //? digunakan dia boleh null
+// //String? jurusan;
+// // Untuk di isi nanti
+//   late String jurusan;
 
-  jurusan = "TI";
-//   ! digunakan untuk memaksa dijalnkan di run / yakin ada datanya
-  print(jurusan.length);
+//   jurusan = "TI";
+// //   ! digunakan untuk memaksa dijalnkan di run / yakin ada datanya
+//   print(jurusan.length);
 
-//   perulangan looping
-//   for plus
-  for (int no = 1; no <= 5; no++) {
-    print(no);
+// //   perulangan looping
+// //   for plus
+//   for (int no = 1; no <= 5; no++) {
+//     print(no);
+//   }
+
+// //   for minus
+//   for (int no = 5; no >= 1; no--) {
+//     print(no);
+//   }
+
+// //   while
+//   int no1 = 1;
+//   int no2 = 5;
+//   while (no1 <= no2) {
+//     print(no1);
+//     no1++;
+//   }
+
+// //   do while
+//   int no3 = 1;
+//   int no4 = 5;
+//   do {
+//     print(no3);
+//     no3++;
+//   } while (no3 <= no4);
+// }
+
+
+
+//oop
+//class
+class Kendaraan {
+  String? merk;
+  String? nama;
+  int? kecepatan;
+
+  //construktor
+  Kendaraan({this.merk, this.nama, this.kecepatan});
+
+  //method
+  maju(int tambahKecepatan) {
+    kecepatan = kecepatan! + tambahKecepatan;
+//     print(kecepatan! + tambahKecepatan);
   }
-
-//   for minus
-  for (int no = 5; no >= 1; no--) {
-    print(no);
-  }
-
-//   while
-  int no1 = 1;
-  int no2 = 5;
-  while (no1 <= no2) {
-    print(no1);
-    no1++;
-  }
-
-//   do while
-  int no3 = 1;
-  int no4 = 5;
-  do {
-    print(no3);
-    no3++;
-  } while (no3 <= no4);
 }
+
+//inheritance / pewarisan
+class Sedan extends Kendaraan {
+  int? jumlahPintu;
+  int? kecepatanMaksimal;
+
+  Sedan({String? merk, this.jumlahPintu, this.kecepatanMaksimal})
+      : super(merk: merk);
+}
+
+void main() {
+  //instansiasi
+  var k1 = Kendaraan(merk: "BMW", nama: "civic", kecepatan: 20);
+
+  k1.merk = "Toyota";
+//   print(k1.maju(40));
+  print(k1.merk);
+  print(k1.nama);
+  print(k1.kecepatan);
+
+  var k2 = Kendaraan(merk: "Mitsubishi", nama: "colt", kecepatan: 40);
+  
+  k2.maju(90);
+//   print(kecepatan);
+  print(k2.merk);
+  print(k2.nama);
+  print(k2.kecepatan);
+
+  var s1 = Sedan(jumlahPintu: 4, kecepatanMaksimal: 120, merk: "Honda");
+  
+  print("~~~~~~~~~~~~~~~");
+  print(s1.jumlahPintu);
+  print(s1.kecepatanMaksimal);
+  print(s1.merk);
+}
+
 
 
 
